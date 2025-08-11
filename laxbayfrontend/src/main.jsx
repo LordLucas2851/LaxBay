4934b6ce4bbe3f518ac6272ab0cf6961fc63dfda
LordLucas2851 <lucas.shaver@lander.edu>
2025-08-10 20:39:04 -0400

    Update main.jsx

--- laxbayfrontend/src/main.jsx
+++ laxbayfrontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ErrorBoundary } from "./components/ErrorBoundary";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ErrorBoundary>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
