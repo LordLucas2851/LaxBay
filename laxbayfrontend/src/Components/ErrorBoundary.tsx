0e346152af9e980b9b20c6cbd4782ff746aed828
LordLucas2851 <lucas.shaver@lander.edu>
2025-08-10 20:38:35 -0400

    Delete laxbayfrontend/src/Components/ErrorBoundary.tsx

--- laxbayfrontend/src/Components/ErrorBoundary.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, ReactNode } from "react";
-
-export class ErrorBoundary extends Component<{ children: ReactNode }, { hasError: boolean }> {
-  constructor(props:any){ super(props); this.state={ hasError:false }; }
-  static getDerivedStateFromError(){ return { hasError: true }; }
-  componentDidCatch(err:any, info:any){ console.error("App crashed:", err, info); }
-  render(){ return this.state.hasError ? <div className="p-6">Something went wrong. Try refreshing.</div> : this.props.children; }
-}
